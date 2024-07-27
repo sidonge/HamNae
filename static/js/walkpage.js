39bf9de477a0aba613c6375102ec057563aaca3c
shinjiaaa <134384192+shinjiaaa@users.noreply.github.com>
2024-07-27 23:11:54 +0900

    캐릭터 선택 페이지 제외 전체 수정 최종안

--- static/js/walkpage.js
+++ static/js/walkpage.js
@@ -1,8 +1,4 @@
 // nav.js
-document.getElementById('menuIcon').addEventListener('click', function() {
-    window.location.href = 'nav.html';
-});
-
 document.addEventListener('DOMContentLoaded', () => {
     const pageContent = document.documentElement.outerHTML;
     localStorage.setItem('pageContent', pageContent);
