a521c7060228afd86e231e876d93eafb15579169
shinjiaaa <134384192+shinjiaaa@users.noreply.github.com>
2024-08-03 14:52:29 +0900

    home 아직 해결 못함

--- services/character.py
+++ services/character.py
@@ -1,9 +1,17 @@
 from fastapi import APIRouter, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse, FileResponse
 from config import templates
+import os
 
 router = APIRouter()
 
 @router.get("/character", response_class=HTMLResponse)
 async def read_character(request: Request):
     return templates.TemplateResponse("character.html", {"request": request})
+
+@router.get("/models/{model_name}")
+async def get_model(model_name: str):
+    file_path = os.path.join("templates", "models", model_name)
+    if os.path.exists(file_path):
+        return FileResponse(file_path)
+    return JSONResponse(content={"success": False, "error": "File not found"}, status_code=404)
