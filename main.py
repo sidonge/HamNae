3a4ef585e58ae346ce0a9c57a10108c921797a15
SEOUIJEONG <ssyj690@gmail.com>
2024-08-04 00:40:37 +0900

    chat 주석 원래대로

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ app.include_router(character.router)
 app.include_router(quest.router)
 app.include_router(walkpage.router)
 app.include_router(petlist.router)
-# app.include_router(chat.router)
+app.include_router(chat.router)
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -45,69 +45,69 @@ async def main(request: Request):
 async def home(request: Request):
     return templates.TemplateResponse("home.html", {"request": request})
 
-# @app.get("/walkpage", response_class=HTMLResponse)
-# async def quest(request: Request):
-#     return templates.TemplateResponse("walkpage.html", {"request": request})
+@app.get("/walkpage", response_class=HTMLResponse)
+async def quest(request: Request):
+    return templates.TemplateResponse("walkpage.html", {"request": request})
 
-# @app.get("/petslist")
-# async def petslist(request: Request):
-#     return templates.TemplateResponse("petslist.html", {"request": request})
+@app.get("/petslist")
+async def petslist(request: Request):
+    return templates.TemplateResponse("petslist.html", {"request": request})
 
 @app.get("/quest", response_class=HTMLResponse)
 async def quest(request: Request):
     return templates.TemplateResponse("quest.html", {"request": request})
 
-# @app.get("/quest_status")
-# async def quest_status():
-#     status = get_status()
-#     return JSONResponse(content={"status": status}, status_code=200)
+@app.get("/quest_status")
+async def quest_status():
+    status = get_status()
+    return JSONResponse(content={"status": status}, status_code=200)
 
-# @app.post("/upload")
-# async def upload_file(file: UploadFile = File(...)):
-#     try:
-#         mission = file.filename.split('_')[0].lower()
-#         new_filename = STAMP_MAP.get(mission, file.filename)
-#         file_location = os.path.join(UPLOAD_DIR, new_filename)
-#         with open(file_location, "wb") as f:
-#             shutil.copyfileobj(file.file, f)
-#         save_status(mission)
-#         return JSONResponse(content={"success": True, "file_path": file_location}, status_code=200)
-#     except Exception as e:
-#         return JSONResponse(content={"success": False, "error": str(e)}, status_code=500)
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(...)):
+    try:
+        mission = file.filename.split('_')[0].lower()
+        new_filename = STAMP_MAP.get(mission, file.filename)
+        file_location = os.path.join(UPLOAD_DIR, new_filename)
+        with open(file_location, "wb") as f:
+            shutil.copyfileobj(file.file, f)
+        save_status(mission)
+        return JSONResponse(content={"success": True, "file_path": file_location}, status_code=200)
+    except Exception as e:
+        return JSONResponse(content={"success": False, "error": str(e)}, status_code=500)
 
-# @app.post("/update_quest_stamps")
-# async def update_quest_stamps(request: Request):
-#     try:
-#         data = await request.json()
-#         mission = data.get("mission")
-#         if mission:
-#             save_status(mission)
-#             return JSONResponse(content={"success": True}, status_code=200)
-#         else:
-#             return JSONResponse(content={"success": False, "error": "Mission not provided"}, status_code=400)
-#     except Exception as e:
-#         return JSONResponse(content={"success": False, "error": str(e)}, status_code=500)
+@app.post("/update_quest_stamps")
+async def update_quest_stamps(request: Request):
+    try:
+        data = await request.json()
+        mission = data.get("mission")
+        if mission:
+            save_status(mission)
+            return JSONResponse(content={"success": True}, status_code=200)
+        else:
+            return JSONResponse(content={"success": False, "error": "Mission not provided"}, status_code=400)
+    except Exception as e:
+        return JSONResponse(content={"success": False, "error": str(e)}, status_code=500)
 
-# @app.get("/", response_class=HTMLResponse)
-# async def get_chat(request: Request):
-#     return templates.TemplateResponse("chat.html", {"request": request})
+@app.get("/", response_class=HTMLResponse)
+async def get_chat(request: Request):
+    return templates.TemplateResponse("chat.html", {"request": request})
 
 
-# @app.post("/api/chat")
-# async def api_chat(message: Message):
-#     try:
-#         response = chat.send_message(message.message)
-#         logging.debug(f"Full response object: {response}")
-#         response_content = response.text if hasattr(
-#             response, 'text') else str(response)
+@app.post("/api/chat")
+async def api_chat(message: Message):
+    try:
+        response = chat.send_message(message.message)
+        logging.debug(f"Full response object: {response}")
+        response_content = response.text if hasattr(
+            response, 'text') else str(response)
 
-#         response_data = jsonable_encoder({
-#             "response": response_content,
-#         })
-#         return JSONResponse(content=response_data)
-#     except Exception as e:
-#         logging.error(f"Exception during chat processing: {e}")
-#         return JSONResponse(status_code=500, content={"detail": str(e)})
+        response_data = jsonable_encoder({
+            "response": response_content,
+        })
+        return JSONResponse(content=response_data)
+    except Exception as e:
+        logging.error(f"Exception during chat processing: {e}")
+        return JSONResponse(status_code=500, content={"detail": str(e)})
 
 
 if __name__ == "__main__":
