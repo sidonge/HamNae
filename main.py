e4431a71ebed7282ea2f87392cb97db1814b5570
shinjiaaa <134384192+shinjiaaa@users.noreply.github.com>
2024-08-05 06:47:57 +0900

    title, nav 전체 수정 완료

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
-<<<<<<< HEAD
-=======
-from fastapi.responses import HTMLResponse
->>>>>>> 46d31bdefbd30b137311ff200d9c45af0eb13820
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from typing import Optional
 from auth import login, register
-<<<<<<< HEAD
-from services import quest, home, character, walkpage, petlist, chat, map
-=======
 from services import quest, home, character, walkpage, petlist, chat, map, mainpage
->>>>>>> 46d31bdefbd30b137311ff200d9c45af0eb13820
 
 app = FastAPI()
 
@@ -32,21 +24,14 @@ templates = Jinja2Templates(directory="templates")
 app.include_router(login.router, prefix="/auth")
 app.include_router(register.router, prefix="/auth")
 
-<<<<<<< HEAD
-=======
 app.include_router(mainpage.router)
->>>>>>> 46d31bdefbd30b137311ff200d9c45af0eb13820
 app.include_router(map.router)
 app.include_router(home.router)
 app.include_router(character.router)
 app.include_router(quest.router)
 app.include_router(walkpage.router)
 app.include_router(petlist.router)
-<<<<<<< HEAD
 #app.include_router(chat.router)
-=======
-# app.include_router(chat.router)
->>>>>>> 46d31bdefbd30b137311ff200d9c45af0eb13820
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -56,8 +41,4 @@ async def get_loading(request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-<<<<<<< HEAD
     uvicorn.run(app, host="127.0.0.1", port=8000)
-=======
-    uvicorn.run(app, host="127.0.0.1", port=8000)
->>>>>>> 46d31bdefbd30b137311ff200d9c45af0eb13820
